105b47e126fd1691b39584ffc98b894e4f422c3c
Your Name <you@example.com>
2017-12-25 12:06:53 +0800

    create pro

---
+++ src/main.js
@@ -0,0 +1,19 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import Fastclick from 'fastclick'
+import 'style/base/reset.css'
+import 'style/base/border.css'
+
+Vue.config.productionTip = false
+Fastclick.attach(document.body)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App }
+})
